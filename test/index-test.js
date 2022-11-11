ee6aa29035bb27240542ba773a43b70d8063c934
Ebyan Jamal <Ebyanjamal6@gmail.com>
2022-11-11 15:49:46 -0700

    commit tree bb9611f3078dc9c17cdcea71e4a66f6997657b36

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
+    expect(shout('hello')).toEqual('HELLO'); function shout(string){return string.toUpperCase();}
   })
 })
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello');
+    expect(whisper('HELLO')).toEqual('hello'); function whisper(string){return string.toLowerCase();}
   })
 })
 
